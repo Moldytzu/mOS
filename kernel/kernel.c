7b9b1097bc94132262f1f6af2cb7af684fabc455
Moldytzu <m.alex123@yahoo.com>
2022-03-15 16:47:46 +0200

    parsing the memory map

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -42,7 +42,7 @@ void _start(struct stivale2_struct *stivale2_struct) {
     framebufferWrite("Initializing the Memory Manager...");
     mmInit();
     framebufferWrite("done\n");
-
+    
     // cause an intrerrupt
     asm volatile ("int $0");
 
--- kernel/mm.c
+++ kernel/mm.c
@@ -1,8 +1,31 @@
 #include <mm.h>
+#include <framebuffer.h>
 
 struct stivale2_struct_tag_memmap *map;
 
+uint64_t totalMem = 0; // total memory
+uint64_t availableMem = 0; // available memory
+uint64_t usedMem = 0; // used memory
+void *baseMem = 0; // base address of the physical memory
+
 void mmInit()
 {
-    map = bootloaderGetMemMap();
+    map = bootloaderGetMemMap(); // get the map
+
+    for(uint64_t i = 0; i < map->entries; i++)
+    {
+        if (totalMem < map->memmap[i].length && map->memmap[i].type == STIVALE2_MMAP_USABLE) // check if this is the longest strip of memory and if it's usable
+        {
+            totalMem = availableMem = map->memmap[i].length; // if yes set the total memory and available memory to the length of the strip
+            baseMem = (void*)map->memmap[i].base; // and set the base memory address
+        }
+    }
+
+    // debug info
+    framebufferWrite("\n\nstart debug info");
+    framebufferWrite("\nmap->entries = ");
+    framebufferWrite(to_string(map->entries));
+    framebufferWrite("\navailableMem = ");
+    framebufferWrite(to_string(availableMem));
+    framebufferWrite("\nend debug info\n\n");
 }
