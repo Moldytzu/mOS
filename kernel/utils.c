a8ba33bd7bac4c34c4dc68bcb01cd3985a5786b5
Moldytzu <m.alex123@yahoo.com>
2022-03-16 17:54:38 +0200

    optimize memset

--- kernel/utils.c
+++ kernel/utils.c
@@ -17,25 +17,25 @@ void hang()
 
 void memset(void *dest, uint8_t data, size_t count)
 {
-    for (size_t i = 0; i < count; i++, dest++)
+    for (; count; count--, dest++)
         *(uint8_t *)dest = data;
 }
 
 void memset16(void *dest, uint16_t data, size_t count)
 {
-    for (size_t i = 0; i < count; i++, dest+=sizeof(uint16_t))
+    for (; count; count--, dest+=sizeof(uint16_t))
         *(uint16_t *)dest = data;
 }
 
 void memset32(void *dest, uint32_t data, size_t count)
 {
-    for (size_t i = 0; i < count; i++, dest+=sizeof(uint32_t))
+    for (; count; count--, dest+=sizeof(uint32_t))
         *(uint32_t *)dest = data;
 }
 
 void memset64(void *dest, uint64_t data, size_t count)
 {
-    for (size_t i = 0; i < count; i++, dest+=sizeof(uint64_t))
+    for (; count; count--, dest+=sizeof(uint64_t))
         *(uint64_t *)dest = data;
 }
 
