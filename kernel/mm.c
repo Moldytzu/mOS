5a8e5272780378668e70ce175bd097029bffaf3c
Moldytzu <m.alex123@yahoo.com>
2022-03-19 20:29:52 +0200

    allocate and deallocate from pool

--- kernel/mm.c
+++ kernel/mm.c
@@ -5,23 +5,44 @@ struct mm_pool pools[0xFFFF]; // 16k pools should be enough
 
 struct stivale2_struct_tag_memmap *map;
 
-bool loop = false;
-void *mmAllocatePage()
+void mmDeallocatePagePool(struct mm_pool pool, void *address)
 {
-    while (pools[0].bitmapByte != pools[0].allocableBase) // loop thru all the bytes in the bitmap
+    uint8_t *byte = pool.base; // byte in the bitmap
+    size_t i = 0;              // index
+
+    while (byte != pool.allocableBase) // loop thru all the bytes in the bitmap
     {
         for (int j = 0; j < 8; j++)
         {
-            if ((0b10000000 >> j) & *pools[0].bitmapByte) // if there is a bit set, it means that there is a page available
+            if ((void *)(pool.allocableBase + i * 4096) == address) // check if we indexed the address
             {
-                pools[0].available -= 4096;                                        // decrement the available memory by a page
-                *pools[0].bitmapByte &= ~(0b10000000 >> j);                        // set that bit
-                loop = false;                                                  // indicate that we are done
-                return (void *)(pools[0].allocableBase + pools[0].bitmapIndex * 4096); // return the address
+                *byte |= 0b10000000 >> j; // set that bit
+                pool.available += 4096;   // increase available memory
+                return;                   // return
             }
-            pools[0].bitmapIndex++; // increase page index in memory
+            i++; // increase page index in memory
         }
-        pools[0].bitmapByte++; // increase byte in bitmap
+        byte++; // increase byte in bitmap
+    }
+}
+
+bool loop = false;
+void *mmAllocatePagePool(struct mm_pool pool)
+{
+    while (pool.bitmapByte != pool.allocableBase) // loop thru all the bytes in the bitmap
+    {
+        for (int j = 0; j < 8; j++)
+        {
+            if ((0b10000000 >> j) & *pool.bitmapByte) // if there is a bit set, it means that there is a page available
+            {
+                pool.available -= 4096;                                        // decrement the available memory by a page
+                *pool.bitmapByte &= ~(0b10000000 >> j);                        // set that bit
+                loop = false;                                                  // indicate that we are done
+                return (void *)(pool.allocableBase + pool.bitmapIndex * 4096); // return the address
+            }
+            pool.bitmapIndex++; // increase page index in memory
+        }
+        pool.bitmapByte++; // increase byte in bitmap
     }
 
     // if we don't find an available page search again from the begining
@@ -32,30 +53,22 @@ void *mmAllocatePage()
         return NULL;
     }
 
-    pools[0].bitmapByte = pools[0].base; // reset byte
+    pool.bitmapByte = pool.base; // reset byte
     loop = true;                 // indicate that we are using recursivity
     return mmAllocatePage();
 }
 
+
+void *mmAllocatePage()
+{
+    // todo: select pool from the array
+    return mmAllocatePagePool(pools[0]);
+}
+
 void mmDeallocatePage(void *address)
 {
-    uint8_t *byte = pools[0].base; // byte in the bitmap
-    size_t i = 0;              // index
-
-    while (byte != pools[0].allocableBase) // loop thru all the bytes in the bitmap
-    {
-        for (int j = 0; j < 8; j++)
-        {
-            if ((void *)(pools[0].allocableBase + i * 4096) == address) // check if we indexed the address
-            {
-                *byte |= 0b10000000 >> j; // set that bit
-                pools[0].available += 4096;   // increase available memory
-                return;                   // return
-            }
-            i++; // increase page index in memory
-        }
-        byte++; // increase byte in bitmap
-    }
+    // todo: select pool from the array
+    mmDeallocatePagePool(pools[0],address);
 }
 
 void mmInit()
