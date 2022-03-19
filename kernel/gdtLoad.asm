b2133442a542ff62b346eede0e34561ac3d83b1f
Moldytzu <m.alex123@yahoo.com>
2022-03-19 19:24:39 +0200

    label segments

--- kernel/gdtLoad.asm
+++ kernel/gdtLoad.asm
@@ -4,14 +4,14 @@ global gdtLoad
 
 gdtLoad:
     lgdt [rdi] ; load gdt from the first argument
-    mov ax, 16
+    mov ax, (8*2) ; 2nd segment, kernel data
     mov ds, ax
     mov es, ax
     mov fs, ax
     mov gs, ax
     mov ss, ax
     pop rdi
-    mov rax, 8
+    mov rax, (8*1) ; 1st segment, kernel code
     push rax
     push rdi
     retfq
--- kernel/tssLoad.asm
+++ kernel/tssLoad.asm
@@ -3,6 +3,6 @@ bits 64
 global tssLoad
 
 tssLoad:
-    mov ax, 40
+    mov ax, (8*5) ; 5th segment, kernel code
     ltr ax ; load tss from offset 40 in the gdt
     ret
