598a916a785574fabccbc89f3fa5f0f0c48a93b0
Moldytzu <m.alex123@yahoo.com>
2022-03-29 20:19:37 +0300

    task structure

--- kernel/scheduler.c
+++ kernel/scheduler.c
@@ -1,5 +1,8 @@
 #include <scheduler.h>
 
+struct sched_task tasks[0x1000]; // todo: replace this with a linked list
+uint16_t lastTID = 0; // last task ID
+
 void schedulerSchedule(struct idt_intrerrupt_stack *stack)
 {
     // todo: swap the registers and the paging table
--- kernel/scheduler.h
+++ kernel/scheduler.h
@@ -1,6 +1,19 @@
 #pragma once
 #include <utils.h>
 #include <idt.h>
+#include <vmm.h>
+
+#define TASK_STATE_RUNNING 0
+#define TASK_STATE_PAUSED 1
+
+struct pack sched_task
+{
+    char name[128];                              // 127 characters should be enough
+    uint8_t state;                               // current state
+    uint16_t tid;                                // task ID
+    struct vmm_page_table *pageTable;            // pointer to the page table
+    struct idt_intrerrupt_stack intrerruptStack; // the last intrerrupt stack
+};
 
 void schedulerSchedule(struct idt_intrerrupt_stack *stack);
 void schedulerInit();
