b29a3502cd659b79ae61340dbacea757987b4c90
Moldytzu <m.alex123@yahoo.com>
2022-03-29 21:54:19 +0300

    failing to map everything correctly

--- kernel/scheduler.c
+++ kernel/scheduler.c
@@ -12,8 +12,10 @@ void schedulerSchedule(struct idt_intrerrupt_stack *stack)
     if (!enabled)
         return; // don't do anything if it isn't enabled
 
-    vmmSwap(tasks[0].pageTable); // load the page table
-    memcpy(stack,&tasks[0].intrerruptStack,sizeof(struct idt_intrerrupt_stack)); // copy the registers
+    vmmSwap(tasks[0].pageTable);                                                   // load the page table
+    memcpy(stack, &tasks[0].intrerruptStack, sizeof(struct idt_intrerrupt_stack)); // copy the registers
+
+    printk("new: cs=%d ss=%d rip=%p", stack->cs, stack->ss, stack->rip);
     // todo: swap the registers and the paging table
 }
 
@@ -36,24 +38,24 @@ void schdulerAdd(const char *name, void *entry, uint64_t stackSize, void *execBa
     memcpy(tasks[index].name, (char *)name, strlen(name)); // set the name
 
     // page table
-    struct vmm_page_table *newTable = mmAllocatePage();             // allocate a new page table
-    memcpy64(newTable, vmmGetBaseTable(), 4096 / sizeof(uint64_t)); // copy the base page table over our new table
-    tasks[index].pageTable = newTable;                              // set the new page table
+    struct vmm_page_table *newTable = mmAllocatePages(4); // allocate a new page table
+    tasks[index].pageTable = newTable;                    // set the new page table
 
     void *stack = mmAllocatePages(stackSize / 4096); // allocate stack for the task
-    for (size_t i = 0; i < stackSize; i += 4096)        // map stack as user, read-write
+
+    for (size_t i = 0; i < stackSize; i += 4096)     // map stack as user, read-write
         vmmMap(newTable, stack + i, stack + i, true, true);
 
     for (size_t i = 0; i < execSize; i += 4096) // map executable as user, read-write
         vmmMap(newTable, execBase + i, execBase + i, true, true);
 
+    vmmMap(newTable, kernelStack, kernelStack, false, true); // map kernel stack as read-write
+
     // initial registers
     tasks[index].intrerruptStack.rip = (uint64_t)entry;               // set the entry point a.k.a the instruction pointer
-    tasks[index].intrerruptStack.rflags = 0x002;                      // rflags, enable intrerrupts
+    tasks[index].intrerruptStack.rflags = 0x002;                      // rflags, disable intrerrupts
     tasks[index].intrerruptStack.krsp = (uint64_t)kernelStack + 4096; // kernel stack
     tasks[index].intrerruptStack.rsp = (uint64_t)stack + stackSize;   // task stack
-    tasks[index].intrerruptStack.cs = 8 * 3;                          // code segment for userspace is the 3rd
-    tasks[index].intrerruptStack.ss = 8 * 4;                          // data segment for userspace is the 4th
-
-    // todo: map the executable
+    tasks[index].intrerruptStack.cs = 8 * 1;                          // code segment for kernelspace is the 3rd
+    tasks[index].intrerruptStack.ss = 8 * 2;                          // data segment for kernelspace is the 4th
 }
