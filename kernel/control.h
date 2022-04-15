c5581487dbb165619b63add7fe891e453e99ece0
Moldytzu <m.alex123@yahoo.com>
2022-04-15 15:51:07 +0300

    saving some clock cycles per table swap

--- kernel/control.h
+++ kernel/control.h
@@ -1,4 +1,13 @@
 #include <utils.h>
 
-void controlLoadCR4(uint64_t value);
-void controlLoadCR3(uint64_t value);
+// load new cr4
+static inline __attribute__((always_inline)) void controlLoadCR4(uint64_t value)
+{
+    iasm("mov %0, %%cr4" ::"r"(value));
+}
+
+// load new cr3
+static inline __attribute__((always_inline)) void controlLoadCR3(uint64_t value)
+{
+    iasm("mov %0, %%cr3" ::"r"(value));
+}
