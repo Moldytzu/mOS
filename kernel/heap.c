4c5d6b5514f3e0f506f534a6534d96e81dcb04fe
Moldytzu <m.alex123@yahoo.com>
2022-04-17 21:19:20 +0300

    malloc

--- kernel/heap.c
+++ kernel/heap.c
@@ -4,16 +4,14 @@
 
 struct heap_segment *lastSegment = NULL;
 struct heap_segment *currentSegment = NULL;
-size_t totalMem;
-size_t freeMem;
-size_t usedMem;
 void *end = 0;
 
 void expand(size_t);
+void split(struct heap_segment *, size_t);
 
 void heapInit()
 {
-    end = (void*)HEAP_START;
+    end = (void *)HEAP_START;
     expand(VMM_PAGE); // expand to a page
 }
 
@@ -56,14 +54,57 @@ void expand(size_t size)
     {
         currentSegment = next;
     }
-
-    totalMem += size + sizeof(struct heap_segment);
-    freeMem += size + sizeof(struct heap_segment);
 }
 
 void *malloc(size_t size)
 {
+    if (size == 0)
+        return NULL;
+
+    size = align(size, 0x10);
+
+    while (1) // loop thru each segment
+    {
+        if (!currentSegment->free)
+        {
+            if (!currentSegment->next)
+                break;                             // if we are at the last segment exit out of loop
+            currentSegment = currentSegment->next; // check next segment
+            continue;
+        }
+
+        if (currentSegment->size > size)
+        {
+            split(currentSegment, size);  // split this segment at the needed lenght
+            currentSegment->free = false; // set as busy
+            return (void *)((uint64_t)currentSegment + sizeof(struct heap_segment));
+        }
+        else if (currentSegment->size == size)
+        {
+            currentSegment->free = false; // set as busy
+            return (void *)((uint64_t)currentSegment + sizeof(struct heap_segment));
+        }
+    }
+
     return NULL;
+}
+
+void split(struct heap_segment *segment, size_t size)
+{
+    if (segment->size < size + sizeof(struct heap_segment)) // can't split a small segment at a larger size
+        return;
+
+    struct heap_segment *new = (struct heap_segment *)((uint64_t)segment + sizeof(struct heap_segment) + size);
+    new->free = true;
+    new->size = segment->size - (size + sizeof(struct heap_segment));
+    new->next = segment->next;
+    new->last = segment;
+
+    if (segment->next == NULL) // link the segment if the chain is over
+        lastSegment = new;
+
+    segment->next = new;  // set new segment
+    segment->size = size; // set new size
 }
 
 void *realloc(void *ptr, size_t size)
