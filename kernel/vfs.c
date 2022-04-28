a0a60edbf1b2b2ee17a550722ce6e625c129152f
Moldytzu <m.alex123@yahoo.com>
2022-04-28 10:47:52 +0300

    default node

--- kernel/vfs.c
+++ kernel/vfs.c
@@ -1,6 +1,37 @@
 #include <vfs.h>
 
+struct vfs_node nodes[0xFF]; // todo: replace with linked nodes
+uint16_t lastNode = 0;
+
+struct vfs_fs root;
+
 void vfsInit()
 {
-    
+    memset64(nodes, 0, sizeof(nodes) / sizeof(uint64_t)); // clear the nodes
+    memset64(&root, 0, sizeof(root) / sizeof(uint64_t));  // clear the root filesystem
+
+    // metadata of the rootfs
+    root.name = "rootfs";
+    root.mountName = "/";
+
+    struct vfs_node node;          // the default node
+    node.filesystem = &root;       // rootfs
+    node.parent = NULL;            // no parent
+    memcpy(node.path, ".root", 5); // copy the path ("/.root")
+    vfsAdd(node);
+}
+
+void vfsAdd(struct vfs_node node)
+{
+    nodes[lastNode++] = node; // add the node
+}
+
+void vfsRemove(struct vfs_node *node)
+{
+    memset64(node, 0, sizeof(struct vfs_node) / sizeof(uint64_t)); // clear the node
+}
+
+struct vfs_node *vfsNodes()
+{
+    return nodes;
 }
--- kernel/vfs.h
+++ kernel/vfs.h
@@ -1,4 +1,21 @@
 #pragma once
 #include <utils.h>
 
+struct vfs_fs
+{
+    const char *name;
+    const char *mountName;
+    void *handlers[5]; // open, close, read, write
+};
+
+struct vfs_node
+{
+    struct vfs_node *parent;
+    struct vfs_fs *filesystem;
+    uint8_t path[128];
+};
+
 void vfsInit();
+void vfsAdd(struct vfs_node node);
+void vfsRemove(struct vfs_node *node);
+struct vfs_node *vfsNodes();
