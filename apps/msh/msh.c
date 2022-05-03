1bbe51a5c9bfc8ad6456a6e8646063a54c79cd13
Moldytzu <m.alex123@yahoo.com>
2022-05-03 19:12:04 +0300

    basic buffer

--- apps/msh/msh.c
+++ apps/msh/msh.c
@@ -6,9 +6,9 @@ int main()
 {
     puts("m Shell\n");
 
-    uint64_t *ptr;
-    sys_mem(SYS_MEM_ALLOCATE,(uint64_t)&ptr,0);
-    *ptr = 0xFFFF; // set data
+    char *kBuffer;
+    uint16_t kIdx;
+    sys_mem(SYS_MEM_ALLOCATE,(uint64_t)&kBuffer,0);
 
     // main loop
     while (1)
@@ -22,7 +22,14 @@ int main()
         {
             sys_input(SYS_INPUT_KEYBOARD, &chr); // read a character off the keyboard buffer
             if(chr)
+            {
                 putchar(chr);
+                kBuffer[kIdx++] = chr; // append the character
+            }
         } while (chr != '\n');
+        kIdx = 0;
+
+        puts("You entered ");
+        puts(kBuffer);
     }
 }
