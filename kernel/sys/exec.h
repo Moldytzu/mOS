e67795ef0b7a3e297aacc2cf828f309d5885c2a8
Moldytzu <m.alex123@yahoo.com>
2022-05-14 11:13:02 +0300

    prevent crashing

--- kernel/sys/exec.h
+++ kernel/sys/exec.h
@@ -7,6 +7,9 @@
 // exec (rsi = path, rdx = newTerminal, r8 = pid, r9 = enviroment)
 void exec(uint64_t syscallNumber, uint64_t path, uint64_t newTerminal, uint64_t returnAddress, uint64_t pid, uint64_t ignored, uint64_t enviroment, struct sched_task *task)
 {
+    if(path < TASK_BASE_ADDRESS || pid < alignD(task->intrerruptStack.rsp, 4096)) // prevent a crash
+        return;
+
     struct sched_task *newTask = elfLoad(PHYSICAL(path));
     uint64_t *ret = PHYSICAL(pid);
 
@@ -21,7 +24,7 @@ void exec(uint64_t syscallNumber, uint64_t path, uint64_t newTerminal, uint64_t 
     else
         newTask->terminal = task->terminal; // set the parent's terminal id
 
-    if(enviroment)
+    if(enviroment && enviroment > TASK_BASE_ADDRESS)
          memcpy(newTask->enviroment, PHYSICAL(enviroment), 4096); // copy the enviroment
 
     *ret = newTask->id; // set the pid
