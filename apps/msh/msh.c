8bb667346085d26d47f26d6998d7ca315af62639
Moldytzu <m.alex123@yahoo.com>
2022-05-14 18:44:03 +0300

    taking in account the path when executing

--- apps/msh/msh.c
+++ apps/msh/msh.c
@@ -5,17 +5,23 @@
 #include <string.h>
 #include <stdbool.h>
 
-uint16_t pathLen;
+uint16_t pathLen = 0;
 const char *path;
-bool pathAvailable;
+
+const char *cmdBuffer;
 
 void handleInput(const char *buffer)
 {
     if (strcmp(buffer, "exit") == 0) // exit command
         exit(EXIT_SUCCESS);
 
+    memset((void *)cmdBuffer, 0, 4096);                            // clear the buffer
+    memcpy((void *)(cmdBuffer + pathLen), buffer, strlen(buffer)); // copy the input
+    if (pathLen)
+        memcpy((void *)cmdBuffer, path, pathLen); // copy the path
+
     uint64_t pid, status;
-    sys_exec(buffer, 0, &pid, 0);
+    sys_exec(cmdBuffer, 0, &pid, 0);
 
     do
     {
@@ -27,19 +33,24 @@ int main()
 {
     puts("m Shell\n");
 
+    // keyboard buffer
     char *kBuffer;
     uint16_t kIdx;
     sys_mem(SYS_MEM_ALLOCATE, (uint64_t)&kBuffer, 0);
     assert(kBuffer != NULL); // assert that the buffer is valid
 
+    // enviroment buffer
     const char *enviroment;
     sys_mem(SYS_MEM_ALLOCATE, (uint64_t)&enviroment, 0);
     assert(enviroment != NULL); // assert that the enviroment is valid
 
-    uint64_t PID;
-    sys_pid(0, SYS_PID_GET, &PID); // get the pid
+    // command buffer
+    sys_mem(SYS_MEM_ALLOCATE, (uint64_t)&cmdBuffer, 0);
+    assert(cmdBuffer != NULL); // assert that the buffer is valid
 
-    sys_pid(PID, SYS_PID_GET_ENVIROMENT, (uint64_t *)enviroment);
+    uint64_t PID;
+    sys_pid(0, SYS_PID_GET, &PID);                                // get the pid
+    sys_pid(PID, SYS_PID_GET_ENVIROMENT, (uint64_t *)enviroment); // get the enviroment
 
     // set the start
     path = enviroment;
@@ -48,8 +59,7 @@ int main()
     while (memcmp(path, "PATH=", 5) != 0) // find the path in enviroment
         path++;
 
-    pathAvailable = memcmp(path, "PATH=", 5) == 0; // check if the path is available by comparing again the bytes
-    if (pathAvailable)
+    if (memcmp(path, "PATH=", 5) == 0) // check if the path is available by comparing again the bytes
     {
         path += 5; // skip the PATH= part
         for (pathLen = 0; path[pathLen] != '|'; pathLen++)
@@ -58,7 +68,7 @@ int main()
 
     // print the path just for debugging purposes
     puts("The path is ");
-    for(int i = 0; i < pathLen; i++)
+    for (int i = 0; i < pathLen; i++)
         putchar(path[i]);
     putchar('\n');
 
