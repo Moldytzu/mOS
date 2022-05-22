75b677158ebf67717a7a3216b60ab53edb797853
Moldytzu <m.alex123@yahoo.com>
2022-05-22 15:42:41 +0300

    better open

--- kernel/sys/open.h
+++ kernel/sys/open.h
@@ -7,7 +7,44 @@ void open(uint64_t syscallNumber, uint64_t returnPtr, uint64_t path, uint64_t re
     if (!INBOUNDARIES(returnPtr) || !INBOUNDARIES(path)) // be sure that the arguments are under the stack
         return;
 
+    uint64_t fd;
+    char *input = PHYSICAL(path);
+    char *buffer = mmAllocatePage();
     uint64_t *returnVal = PHYSICAL(returnPtr);
-    uint64_t fd = vfsOpen(PHYSICAL(path)); // open the node
-    *returnVal = fd;                       // return the value
+
+    if (memcmp(input, "./", 2) == 0) // check if the task requests from the current directory
+    {
+        input += 2; // skip ./
+        goto cwd;
+    }
+
+    memset64(buffer, 0, VMM_PAGE / sizeof(uint64_t)); // clear the buffer
+    memcpy(buffer, input, strlen(input));             // copy the input
+
+    // check if it exists
+    fd = vfsOpen(buffer);
+    if (fd)
+    {
+        *returnVal = fd; // return the value
+        goto ret;
+    }
+
+cwd: // copy the cwd before the input
+    uint64_t offset = strlen(task->cwd);
+    memset64(buffer, 0, VMM_PAGE / sizeof(uint64_t));
+    memcpy((void *)(buffer + offset), input, strlen(input));
+    memcpy((void *)buffer, task->cwd, offset);
+
+    // check if it exists
+    fd = vfsOpen(buffer);
+    if (fd)
+    {
+        *returnVal = fd; // return the value
+        goto ret;
+    }
+
+    *returnVal = 0; // fail
+
+ret:
+    mmDeallocatePage(buffer);
 }
