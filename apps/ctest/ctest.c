d9aab34d5c85a48f54fafe8cc4bf1cca8238a829
Moldytzu <m.alex123@yahoo.com>
2022-07-15 13:12:42 +0300

    libc testing suite

---
+++ apps/ctest/ctest.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <assert.h>
+#include <errno.h>
+#include <string.h>
+
+void test(const char *name, bool result)
+{
+    puts(name);
+    puts("...");
+
+    if(result)   
+        puts("ok\n");
+    else
+        puts("fail\n");
+}
+
+int main(int argc, char **argv)
+{
+    puts("Testing libc...\n");
+
+    // assert
+    assert(1 == 1);
+    test("assert", 1 == 1);
+    
+    // errno
+    #ifdef EDOM 
+    #define RESULT 1
+    #else
+    #define RESULT 0
+    #endif
+
+    test("errno", RESULT);
+
+    #undef RESULT
+
+    // stdlib
+    test("abs", abs(-1) == 1 && abs(1) == 1);
+    test("atoi", atoi("123") == 123);
+    test("labs", labs(-1) == 1 && labs(1) == 1);
+    test("atol", atol("123") == 123);
+
+    // stdio
+
+    // string
+    test("memcmp", memcmp("abc", "abc", 3) == 0);
+    test("strlen", strlen("abc") == 3);
+    test("strcmp", strcmp("abc", "abc") == 0);
+}
