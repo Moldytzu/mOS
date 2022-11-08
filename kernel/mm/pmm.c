a65e17275b43b8bccaa3ae985f89829000b543bc
Moldytzu <m.alex123@yahoo.com>
2022-11-08 21:37:51 +0200

    prototype better pmm

--- kernel/mm/pmm.c
+++ kernel/mm/pmm.c
@@ -8,6 +8,34 @@ pmm_pool_t pools[256]; // 256 pools should be enough
 
 void *pmmPage()
 {
+    for (int i = 0; i < poolCount; i++)
+    {
+        // find an available pool
+        if (pools[i].available < 4096)
+            continue;
+
+        pmm_pool_t *pool = &pools[i];
+
+        // iterate over the bitmap bytes to find an available page
+        for (; pool->lastBitmapIndex < pool->bitmapBytes; pool->lastBitmapIndex += sizeof(uint64_t))
+        {
+            for (; pool->lastMaskBit < 64; pool->lastMaskBit++, pool->lastPageIndex++)
+            {
+                uint64_t mask = 0x8000000000000000 >> pool->lastMaskBit;
+                uint64_t *bytes = (uint64_t *)(pool->base + pool->bitmapBytes + pool->lastBitmapIndex);
+
+                if (*bytes & mask) // check against the mask to see if the bit is set
+                    continue;
+
+                *bytes |= mask; // set the bit to mark the page as used
+
+                pool->available -= 4096;
+                pool->used += 4096;
+
+                return (void *)(pool->alloc + 4096 * pool->lastPageIndex);
+            }
+        }
+    }
 }
 
 void *pmmPages(uint64_t pages)
@@ -43,15 +71,16 @@ void pmmInit()
         memset(pool, 0, sizeof(pmm_pool_t));
 
         pool->bitmapBytes = entry->length / 4096 / 8; // we divide the memory regions in pages (4 KiB chunks) then we will store the availability in a bit in the bitmap
-        pool->alloc = (void *)entry->base + pool->bitmapBytes;
+        pool->alloc = (void *)align((void *)entry->base + pool->bitmapBytes, 4096);
         pool->base = (void *)entry->base;
         pool->available = entry->length - pool->bitmapBytes;
     }
 
     for (int i = 0; i < poolCount; i++)
-    {
-        printk("%d: 0x%x -> %d KiB, %d bmp\n", i, pools[i].base, pools[i].available, pools[i].bitmapBytes);
-    }
+        printk("%d: 0x%x -> 0x%x (%d KiB), %d bmp\n", i, pools[i].base, pools[i].available + pools[i].bitmapBytes, pools[i].available, pools[i].bitmapBytes);
+
+    for (int i = 0; i < 100; i++)
+        printk("%x ", pmmPage());
 
     while (1)
         ;
