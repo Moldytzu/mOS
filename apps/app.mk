b84d20212148360662e6235064129d1a769033a6
Moldytzu <m.alex123@yahoo.com>
2022-12-02 15:21:05 +0200

    the os is bootable with kvm but it is very finicky

--- apps/app.mk
+++ apps/app.mk
@@ -16,13 +16,9 @@ INTERNALCFLAGS :=   		 \
 	-mno-red-zone        	 \
 	-Werror 			     \
 	-mcmodel=large           \
-	-msse                    \
-	-msse2                   \
-	-msse3                   \
-	-msse4                   \
-	-msse4.1                 \
-	-msse4.2                 \
-	-O2
+	-march=nocona            \
+	-O2                      \
+	-MMD
 
 # Internal linker flags that should not be changed by the user.
 INTERNALLDFLAGS :=         \
