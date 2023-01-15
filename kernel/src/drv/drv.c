544a18a73e21465b9e6cf289a553368537b20c5f
Moldytzu <m.alex123@yahoo.com>
2023-01-15 19:16:52 +0200

    drv: skeleton for manager

---
+++ kernel/src/drv/drv.c
@@ -0,0 +1,90 @@
+#include <drv/drv.h>
+#include <mm/blk.h>
+#include <sched/scheduler.h>
+
+drv_context_fb_t *fb;
+drv_context_fb_t fbRef;
+uint16_t fbIdx;
+
+void drvInit()
+{
+    // allocate the contexts
+    fb = blkBlock(sizeof(drv_context_fb_t) * DRV_MAX_CONTEXTS);
+    zero(fb, sizeof(drv_context_fb_t) * DRV_MAX_CONTEXTS);
+}
+
+void drvExit(uint32_t drv)
+{
+    // clear contexts used by the drv
+    for (int i = 0; i < DRV_MAX_CONTEXTS; i++)
+    {
+        if (fb[i].pid == drv)
+            zero(&fb[i], sizeof(drv_context_fb_t));
+    }
+
+    printk("drv: %s exits\n", schedulerGet(drv));
+}
+
+void *drvRegister(uint32_t drv, uint32_t type)
+{
+    // return a new context
+    switch (type)
+    {
+
+    case DRV_TYPE_FB:
+    {
+        if (fbIdx == DRV_MAX_CONTEXTS) // hell nah man (todo: reallocate every time)
+            return NULL;
+
+        // generate the context from the active one (if there is)
+        fb[fbIdx].pid = drv;
+        if (drvQueryActive(DRV_TYPE_FB))
+        {
+            drv_context_fb_t *active = drvQueryActive(DRV_TYPE_FB);
+            fb[fbIdx].requestedXres = active->requestedXres;
+            fb[fbIdx].requestedYres = active->requestedYres;
+        }
+
+        return &fb[fbIdx++];
+    }
+
+    default:
+        break;
+    }
+
+    return NULL;
+}
+
+void *drvQueryActive(uint32_t type)
+{
+    // return the most up-to-date context
+    switch (type)
+    {
+    case DRV_TYPE_FB:
+        for (int i = 0; i < fbIdx; i++)
+        {
+            if(fb[fbIdx].requestedXres == fbRef.requestedXres && fb[fbIdx].requestedYres == fbRef.requestedYres)
+                return &fb[fbIdx];
+        }
+
+        break;
+    
+    default:
+        break;
+    }
+
+    return NULL;
+    
+}
+
+void drvSocketCreate(uint32_t drv, uint32_t sockID)
+{
+}
+
+void drvQuerySockets(uint32_t drv, uint32_t *sockets)
+{
+}
+
+void drvQueryContexts(uint32_t type, void **contexts)
+{
+}
---
+++ kernel/src/drv/drv.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <misc/utils.h>
+
+#define DRV_MAX_CONTEXTS 32
+
+#define DRV_TYPE_FB 1
+#define DRV_TYPE_INPUT 2
+
+pstruct
+{
+    uint32_t pid; // driver pid
+    void *base;   // base address
+    uint32_t currentXres, currentYres;
+    uint32_t requestedXres, requestedYres;
+}
+drv_context_fb_t;
+
+void drvInit();
+void drvExit(uint32_t drv);
+void *drvRegister(uint32_t drv, uint32_t type);
+void drvSocketCreate(uint32_t drv, uint32_t sockID);
+void drvQuerySockets(uint32_t drv, uint32_t *sockets);
+void drvQueryContexts(uint32_t type, void **contexts);
+void *drvQueryActive(uint32_t type);
