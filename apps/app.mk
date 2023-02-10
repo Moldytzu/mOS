9f7c4651d29564e4d34684ee965046040327d94e
Moldytzu <m.alex123@yahoo.com>
2023-02-10 15:01:07 +0200

    build: clean up generalize build settings like compiler flags, paths etc.

--- apps/app.mk
+++ apps/app.mk
@@ -1,31 +1,13 @@
-# use cross c compiler
-CC := ~/cross_compiler/bin/x86_64-elf-gcc
-
-# use cross linker
-LD := ~/cross_compiler/bin/x86_64-elf-ld
+include ../../compile.mk
 
 # Internal C flags that should not be changed by the user.
 INTERNALCFLAGS :=   		 \
-	-I.                  	 \
-	-I../../libc/inc         \
-	-std=gnu11           	 \
-	-ffreestanding       	 \
-	-fno-stack-protector 	 \
-	-fno-pic             	 \
-	-mabi=sysv           	 \
-	-mno-red-zone        	 \
-	-Werror 			     \
-	-mcmodel=large           \
-	-march=nocona            \
-	-O2                      \
-	-g                       \
-	-MMD
+	-I../../libc/inc         
 
 # Internal linker flags that should not be changed by the user.
 INTERNALLDFLAGS :=         \
 	-T../app.ld            \
 	-nostdlib              \
-	-zmax-page-size=0x1000 \
 	-static                \
 	../../libc.a
 
---
+++ compile.mk
@@ -0,0 +1,43 @@
+# c compiler
+CC := ~/cross_compiler/bin/x86_64-elf-gcc
+
+# archiver
+AR := ~/cross_compiler/bin/x86_64-elf-ar
+
+# linker
+LD := ~/cross_compiler/bin/x86_64-elf-ld
+
+# assembler
+ASM := nasm
+
+# flags for c compiler
+CFLAGS :=   		         \
+	-I.                  	 \
+	-Iinc/                   \
+	-std=gnu11           	 \
+	-ffreestanding       	 \
+	-fno-stack-protector 	 \
+	-fno-pic             	 \
+	-mabi=sysv           	 \
+	-mno-red-zone        	 \
+	-Werror 			     \
+	-mcmodel=large           \
+	-O2                      \
+	-march=nocona            \
+	-MMD                     \
+	-g
+
+# flags for assembler
+ASMFLAGS := \
+	-felf64 \
+	-gdwarf
+
+# flags for linker
+LDFLAGS := 				   \
+	-zmax-page-size=0x1000 \
+
+# Use find to glob all *.c files in the directory and extract the object names.
+SRCDIR := src
+OBJDIR := obj
+
+rwildcard=$(foreach d,$(wildcard $(1:=/*)),$(call rwildcard,$d,$2) $(filter $(subst *,%,$2),$d))
