5380c82db9ad6fbde532ed81fecaba3b3c301231
Moldytzu <m.alex123@yahoo.com>
2023-02-17 21:12:11 +0200

    log: use a lock to not screw up the colours

--- kernel/src/misc/logger.c
+++ kernel/src/misc/logger.c
@@ -3,6 +3,7 @@
 #include <cpu/smp.h>
 #include <drv/framebuffer.h>
 
+locker_t loggerLock;
 uint32_t logOldColour;
 
 #define PUSH_COLOUR(x)                                             \
@@ -22,115 +23,123 @@ uint32_t logOldColour;
 
 void logInfo(const char *fmt, ...)
 {
-    uint64_t milis = TIME_NANOS_TO_MILIS(timeNanos());
+    lock(loggerLock, {
+        uint64_t milis = TIME_NANOS_TO_MILIS(timeNanos());
 
-    va_list args;
+        va_list args;
 
-    PUSH_COLOUR(0x3050FF);
-    printk("{#%d} (%d.%d): ", smpID(), milis / 1000, milis % 1000 / 100); // do some trickery so we don't use the fpu, display first the seconds part then hundreds of miliseconds
-    POP_COLOUR();
+        PUSH_COLOUR(0x3050FF);
+        printk("{#%d} (%d.%d): ", smpID(), milis / 1000, milis % 1000 / 100); // do some trickery so we don't use the fpu, display first the seconds part then hundreds of miliseconds
+        POP_COLOUR();
 
-    PUSH_COLOUR(0x20BF20);
-    va_start(args, fmt);
-    printk_impl(fmt, args);
-    va_end(args);
-    POP_COLOUR();
+        PUSH_COLOUR(0x20BF20);
+        va_start(args, fmt);
+        printk_impl(fmt, args);
+        va_end(args);
+        POP_COLOUR();
 
-    printk("\n");
+        printk("\n");
 
 #ifdef K_COM_LOG
-    printks("{#%d} (%d.%d): ", smpID(), milis / 1000, milis % 1000 / 100); // do some trickery so we don't use the fpu, display first the seconds part then hundreds of miliseconds
+        printks("{#%d} (%d.%d): ", smpID(), milis / 1000, milis % 1000 / 100); // do some trickery so we don't use the fpu, display first the seconds part then hundreds of miliseconds
 
-    va_start(args, fmt);
-    printks_impl(fmt, args);
-    va_end(args);
+        va_start(args, fmt);
+        printks_impl(fmt, args);
+        va_end(args);
 
-    printks("\n");
+        printks("\n");
 #endif
+    });
 }
 
 void logWarn(const char *fmt, ...)
 {
-    va_list args;
-    uint64_t milis = TIME_NANOS_TO_MILIS(timeNanos());
+    lock(loggerLock, {
+        va_list args;
+        uint64_t milis = TIME_NANOS_TO_MILIS(timeNanos());
 
-    PUSH_COLOUR(0x3050FF);
-    printk("{#%d} (%d.%d): ", smpID(), milis / 1000, milis % 1000 / 100); // do some trickery so we don't use the fpu, display first the seconds part then hundreds of miliseconds
-    POP_COLOUR();
+        PUSH_COLOUR(0x3050FF);
+        printk("{#%d} (%d.%d): ", smpID(), milis / 1000, milis % 1000 / 100); // do some trickery so we don't use the fpu, display first the seconds part then hundreds of miliseconds
+        POP_COLOUR();
 
-    PUSH_COLOUR(0xFFA000);
-    va_start(args, fmt);
-    printk_impl(fmt, args);
-    va_end(args);
-    POP_COLOUR();
+        PUSH_COLOUR(0xFFA000);
+        va_start(args, fmt);
+        printk_impl(fmt, args);
+        va_end(args);
+        POP_COLOUR();
 
-    printk("\n");
+        printk("\n");
 
 #ifdef K_COM_LOG
-    printks("{#%d} (%d.%d): ", smpID(), milis / 1000, milis % 1000 / 100); // do some trickery so we don't use the fpu, display first the seconds part then hundreds of miliseconds
+        printks("{#%d} (%d.%d): ", smpID(), milis / 1000, milis % 1000 / 100); // do some trickery so we don't use the fpu, display first the seconds part then hundreds of miliseconds
 
-    va_start(args, fmt);
-    printks_impl(fmt, args);
-    va_end(args);
+        va_start(args, fmt);
+        printks_impl(fmt, args);
+        va_end(args);
 
-    printks("\n");
+        printks("\n");
 #endif
+    });
 }
 
 void logError(const char *fmt, ...)
 {
-    va_list args;
-    uint64_t milis = TIME_NANOS_TO_MILIS(timeNanos());
+    lock(loggerLock, {
+        va_list args;
+        uint64_t milis = TIME_NANOS_TO_MILIS(timeNanos());
 
-    PUSH_COLOUR(0x3050FF);
-    printk("{#%d} (%d.%d): ", smpID(), milis / 1000, milis % 1000 / 100); // do some trickery so we don't use the fpu, display first the seconds part then hundreds of miliseconds
-    POP_COLOUR();
+        PUSH_COLOUR(0x3050FF);
+        printk("{#%d} (%d.%d): ", smpID(), milis / 1000, milis % 1000 / 100); // do some trickery so we don't use the fpu, display first the seconds part then hundreds of miliseconds
+        POP_COLOUR();
 
-    PUSH_COLOUR(0xFF2020);
-    va_start(args, fmt);
-    printk_impl(fmt, args);
-    va_end(args);
-    POP_COLOUR();
+        PUSH_COLOUR(0xFF2020);
+        va_start(args, fmt);
+        printk_impl(fmt, args);
+        va_end(args);
+        POP_COLOUR();
 
-    printk("\n");
+        printk("\n");
 
 #ifdef K_COM_LOG
-    printks("{#%d} (%d.%d): ", smpID(), milis / 1000, milis % 1000 / 100); // do some trickery so we don't use the fpu, display first the seconds part then hundreds of miliseconds
+        printks("{#%d} (%d.%d): ", smpID(), milis / 1000, milis % 1000 / 100); // do some trickery so we don't use the fpu, display first the seconds part then hundreds of miliseconds
 
-    va_start(args, fmt);
-    printks_impl(fmt, args);
-    va_end(args);
+        va_start(args, fmt);
+        printks_impl(fmt, args);
+        va_end(args);
 
-    printks("\n");
+        printks("\n");
 #endif
+    });
 }
 
 void logDbg(int level, const char *fmt, ...)
 {
-    // todo: use level as a level of verbosity by comparing with the value set in config.h and returning if it is higher than that
+    lock(loggerLock, {
+        // todo: use level as a level of verbosity by comparing with the value set in config.h and returning if it is higher than that
 
-    va_list args;
-    uint64_t milis = TIME_NANOS_TO_MILIS(timeNanos());
+        va_list args;
+        uint64_t milis = TIME_NANOS_TO_MILIS(timeNanos());
 
-    PUSH_COLOUR(0x00FF00);
+        PUSH_COLOUR(0x00FF00);
 
-    printk("{#%d} (%d.%d): ", smpID(), milis / 1000, milis % 1000 / 100); // do some trickery so we don't use the fpu, display first the seconds part then hundreds of miliseconds
+        printk("{#%d} (%d.%d): ", smpID(), milis / 1000, milis % 1000 / 100); // do some trickery so we don't use the fpu, display first the seconds part then hundreds of miliseconds
 
-    va_start(args, fmt);
-    printk_impl(fmt, args);
-    va_end(args);
+        va_start(args, fmt);
+        printk_impl(fmt, args);
+        va_end(args);
 
-    printk("\n");
+        printk("\n");
 
-    POP_COLOUR();
+        POP_COLOUR();
 
 #ifdef K_COM_LOG
-    printks("{#%d} (%d.%d): ", smpID(), milis / 1000, milis % 1000 / 100); // do some trickery so we don't use the fpu, display first the seconds part then hundreds of miliseconds
+        printks("{#%d} (%d.%d): ", smpID(), milis / 1000, milis % 1000 / 100); // do some trickery so we don't use the fpu, display first the seconds part then hundreds of miliseconds
 
-    va_start(args, fmt);
-    printks_impl(fmt, args);
-    va_end(args);
+        va_start(args, fmt);
+        printks_impl(fmt, args);
+        va_end(args);
 
-    printks("\n");
+        printks("\n");
 #endif
+    });
 }
