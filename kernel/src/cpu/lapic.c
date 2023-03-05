cecb319ba87aaa5748466711335039dada786162
Moldytzu <m.alex123@yahoo.com>
2023-03-05 18:16:34 +0200

    lapic: read/write from/to registers

--- kernel/src/cpu/lapic.c
+++ kernel/src/cpu/lapic.c
@@ -5,9 +5,24 @@
 #include <misc/logger.h>
 #include <mm/vmm.h>
 
+void lapicWrite(uint64_t offset, uint32_t value)
+{
+    *((uint32_t *)((uint8_t *)lapicBase() + offset)) = value;
+}
+
+uint32_t lapicRead(uint64_t offset)
+{
+    return *((uint32_t *)((uint8_t *)lapicBase() + offset));
+}
+
 void *lapicBase()
 {
     return (void *)(rdmsr(MSR_APIC_BASE) & 0xFFFFFFFFFFFFF000); // clear first 11 bits
+}
+
+void lapicEOI()
+{
+    lapicWrite(APIC_REG_EOI, 0);
 }
 
 void lapicInit()
@@ -24,6 +39,8 @@ void lapicInit()
     uint32_t high = (uint64_t)lapicBase() | 0b100100000000; // set global enable flag and processor is bsp flag
 
     wrmsr(MSR_APIC_BASE, low, high); // write back the base
+
+    lapicWrite(APIC_REG_SPURIOUS_INT_VECTOR, lapicRead(APIC_REG_SPURIOUS_INT_VECTOR) | 0b10000000); // set apic software enable/disable
 
     logInfo("lapic: present at %x", lapicBase());
 }
--- kernel/src/cpu/lapic.h
+++ kernel/src/cpu/lapic.h
@@ -2,5 +2,10 @@
 #include <misc/utils.h>
 
 #define MSR_APIC_BASE 0x1B
+#define APIC_REG_SPURIOUS_INT_VECTOR 0xF0
+#define APIC_REG_EOI 0xB0
 
 void lapicInit();
+void *lapicBase();
+void lapicWrite(uint64_t offset, uint32_t value);
+void lapicEOI();
