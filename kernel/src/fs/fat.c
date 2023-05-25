5597f0bafe80f5790d7103a4a8884fa3bc520b0a
Moldytzu <m.alex123@yahoo.com>
2023-05-25 22:41:13 +0300

    fat: support read callback

--- kernel/src/fs/fat.c
+++ kernel/src/fs/fat.c
@@ -19,37 +19,37 @@ bool fatIsValid(fat_bpb_t *bpb)
     return bpb->bootSignature == VFS_MBR_SIGNATURE;
 }
 
-// read handler
-void fatFSRead(struct vfs_node_t *node, void *buffer, uint64_t size, uint64_t offset)
-{
-}
-
-// open handler
-uint8_t fatFSOpen(struct vfs_node_t *node)
-{
-    return 1;
-}
-
 bool fatDirValid(fat_dir_t dir)
 {
-    return dir.name[0] == 0 || dir.name[0] == 0xE5;
+    return dir.name[0] != 0 && dir.name[0] != 0xE5;
 }
 
-// map nodes in the vfs
-void fatMap(struct vfs_node_t *root)
+void fatParseSFN(char *name, fat_dir_t *entry)
 {
-    size_t partitionIdx = CONTEXT(root->filesystem->context)->partition;
-    vfs_partition_t partition = CONTEXT(root->filesystem->context)->drive->partitions[partitionIdx];
-    fat_bpb_t *fs = CONTEXT(root->filesystem->context)->bpb;
-    fat_context_t *context = CONTEXT(root->filesystem->context);
+    int last = 0; // get last character that isn't a space
+    for (; last < 8; last++)
+        if (entry->name[last] == ' ')
+            break;
 
-    if (fs->rootDirectoryEntries != 0 || FAT_CLUSTERS(fs) <= 65526) // not FAT32
-        return;
+    // parse 8.3 file name
+    memcpy(name, entry->name, last);             // copy the text before the extension
+    name[last] = '.';                            // put the extension dot
+    memcpy(name + last + 1, &entry->name[8], 3); // copy the extension
 
+    for (int i = 0; i < 12; i++) // lower all the characters
+        name[i] = tolower(name[i]);
+}
+
+fat_dir_t fatGetEntry(struct vfs_node_t *node)
+{
+    fat_context_t *context = CONTEXT(node->filesystem->context);
+    size_t partitionIdx = context->partition;
+    vfs_partition_t partition = context->drive->partitions[partitionIdx];
+    fat_bpb_t *fs = context->bpb;
     fat_dir_t *entries = pmmPage();
 
     context->drive->read(entries, partition.startLBA + FAT_ROOT_START(fs), 4096 / VFS_SECTOR);
-    for (int i = 0; !fatDirValid(entries[i]); i++)
+    for (int i = 0; fatDirValid(entries[i]); i++)
     {
         fat_dir_t entry = entries[i];
 
@@ -59,31 +59,73 @@ void fatMap(struct vfs_node_t *root)
         if (*(uint8_t *)&entry.attributes == 0xF) // long file name entry (todo: parse that)
             continue;
 
-        int last = 0; // get last character that isn't a space
-        for (; last < 8; last++)
-            if (entry.name[last] == ' ')
-                break;
+        char name[12];
+        zero(name, sizeof(name));
+        fatParseSFN(name, &entry); // parse the name
+
+        if (strcmp(node->path, name) != 0) // compare the names
+            continue;
+
+        pmmDeallocate(entries);
+        return entry;
+    }
+
+    pmmDeallocate(entries);
+    fat_dir_t d;
+    zero(&d, sizeof(d));
+    return d;
+}
+
+// read handler
+void fatFSRead(struct vfs_node_t *node, void *buffer, uint64_t size, uint64_t offset)
+{
+    fat_dir_t entry = fatGetEntry(node);
+    if (!fatDirValid(entry))
+        return;
+
+    fat_context_t *context = CONTEXT(node->filesystem->context);
+    size_t partitionIdx = context->partition;
+    vfs_partition_t partition = context->drive->partitions[partitionIdx];
+    fat_bpb_t *fs = context->bpb;
+
+    size_t sector = partition.startLBA + (CLUSTER(entry) - 2) * fs->sectorsPerCluster + FAT_DATA_START(fs);
+    context->drive->read(buffer, sector, 1); // todo: respect size and offset (todo: read to a buffer)
+}
+
+// open handler
+uint8_t fatFSOpen(struct vfs_node_t *node)
+{
+    return 1;
+}
+
+// map nodes in the vfs
+void fatMap(struct vfs_node_t *root)
+{
+    fat_context_t *context = CONTEXT(root->filesystem->context);
+    size_t partitionIdx = context->partition;
+    vfs_partition_t partition = context->drive->partitions[partitionIdx];
+    fat_bpb_t *fs = context->bpb;
+
+    if (fs->rootDirectoryEntries != 0 || FAT_CLUSTERS(fs) <= 65526) // not FAT32
+        return;
+
+    fat_dir_t *entries = pmmPage();
+
+    context->drive->read(entries, partition.startLBA + FAT_ROOT_START(fs), 4096 / VFS_SECTOR);
+    for (int i = 0; fatDirValid(entries[i]); i++)
+    {
+        fat_dir_t entry = entries[i];
+
+        if (entry.attributes.directory) // we don't support subdirectory traversal yet (todo: do that)
+            continue;
+
+        if (*(uint8_t *)&entry.attributes == 0xF) // long file name entry (todo: parse that)
+            continue;
 
         // parse 8.3 file name
         char name[12];
         zero(name, sizeof(name));
-        memcpy(name, entry.name, last);             // copy the text before the extension
-        name[last] = '.';                           // put the extension dot
-        memcpy(name + last + 1, &entry.name[8], 3); // copy the extension
-
-        for (int i = 0; i < 12; i++) // lower all the characters
-            name[i] = tolower(name[i]);
-
-        char *contents = pmmPage();
-        
-        size_t sector = partition.startLBA + (CLUSTER(entry) - 2) * fs->sectorsPerCluster + FAT_DATA_START(fs);
-        context->drive->read(contents, sector, 1);
-        
-        printks("name: %s; attr: 0x%x; cluster: 0x%x; size: %d b; sector %d\n", name, entry.attributes, CLUSTER(entry), entry.size, sector);
-        
-        for (int j = 0; j < 16; j++)
-            printks("%c", contents[j]);
-        printks("\n");
+        fatParseSFN(name, &entry);
 
         struct vfs_node_t node;             // create a node
         zero(&node, sizeof(node));          // zero it
@@ -92,6 +134,8 @@ void fatMap(struct vfs_node_t *root)
         memcpy(node.path, name, 12);        // copy the name
         vfsAdd(node);
     }
+
+    pmmDeallocate(entries);
 }
 
 // create a new fat context
