498ca5f6e369a056077e397c0f1d7a54f2517070
Moldytzu <m.alex123@yahoo.com>
2023-06-03 22:05:26 +0300

    sys_mem: prevent out of memory

--- kernel/src/sys/mem.c
+++ kernel/src/sys/mem.c
@@ -3,6 +3,7 @@
 
 #define ADDRESSES_IN_PAGES(x) (x * VMM_PAGE / sizeof(uint64_t))
 #define PAGES_IN_ADDRESSES(x) (x * sizeof(uint64_t) / VMM_PAGE)
+#define RESERVED_PAGES 8
 
 // mem (rsi = call, rdx = arg1, r8 = arg2)
 void mem(uint64_t call, uint64_t arg1, uint64_t arg2, uint64_t r9, sched_task_t *task)
@@ -16,6 +17,9 @@ void mem(uint64_t call, uint64_t arg1, uint64_t arg2, uint64_t r9, sched_task_t 
         size_t pages = arg2; // pages to allocate
         if (!pages)
             pages = 1;
+
+        if(pmmTotal().available < (pages + RESERVED_PAGES) * 4096) // let some memory free so we don't crash
+            return;
 
         void *page = pmmPages(pages); // allocate the pages
 
