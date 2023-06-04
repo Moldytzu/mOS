f2fe4c72872db7307eaea77068cc8d9a02e3d436
Moldytzu <m.alex123@yahoo.com>
2023-06-04 19:31:06 +0300

    libc: implement strncpy

--- libc/src/string/strncpy.c
+++ libc/src/string/strncpy.c
@@ -3,6 +3,14 @@
 
 char *strncpy(char *restrict s1,const char *restrict s2, size_t n)
 {
-    puts("strncpy stub\n");
+    for(int i = 0; i < n; i++)
+    {
+        char c = *(s2++);
+
+        if(!c)
+            *(s1++) = 0;
+        else
+            *(s1++) = c;
+    }
     return 0;
 }
