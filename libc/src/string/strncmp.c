0a2ef7bdc293f76a06ef719aa587228b232b7c7b
Moldytzu <m.alex123@yahoo.com>
2023-06-04 19:38:00 +0300

    libc: implement strncmp

--- libc/src/string/strncmp.c
+++ libc/src/string/strncmp.c
@@ -4,6 +4,10 @@
 
 int strncmp(const char *s1, const char *s2, size_t n)
 {
-    puts("strncmp stub\n");
-    return 0;
+    while(*s1 != 0 && *s2 != 0)
+    {
+        if(*s1 != *s2 || !(n--))
+            break;
+    }
+    return *s1 - *s2;
 }
