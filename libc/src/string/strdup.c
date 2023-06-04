47db11fe505ed13bf36e3591835ce3fed7f1adbd
Moldytzu <m.alex123@yahoo.com>
2023-06-04 19:45:43 +0300

    libc: implement strdup

--- libc/src/string/strdup.c
+++ libc/src/string/strdup.c
@@ -1,7 +1,11 @@
 #include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 char *strdup( const char *src )
 {
-    puts("strdup stub\n");
+    void *buffer = malloc(strlen(src) + 1);
+    memset(buffer, 0, strlen(src) + 1);
+    memcpy(buffer, src, strlen(src));
+    return buffer;
 }
