0e4fab7904998ce255f7344e41bebd1e4f68654f
Moldytzu <m.alex123@yahoo.com>
2023-07-14 18:53:32 +0300

    sysinfo: display screen resolution

--- apps/sysinfo/src/sysinfo.c
+++ apps/sysinfo/src/sysinfo.c
@@ -26,6 +26,10 @@ int main(int argc, char **argv)
     sys_perf(SYS_PERF_GET_MEMORY, (uint64_t)&used, (uint64_t)&available);
     sys_perf(SYS_PERF_GET_CPU, (uint64_t)&cores, (uint64_t)&unused);
 
+    uint64_t screenW, screenH;
+    sys_display(SYS_DISPLAY_GET, (uint64_t)&screenW, (uint64_t)&screenH);
+
+    printf("Screen resolution: %dx%d pixels\n", screenW, screenH);
     printf("Used: %d KiB; Available: %d KiB\n", used / 1024, available / 1024);
     printf("CPU %d * %s (%s)\n", cores, name, vendor);
 }
