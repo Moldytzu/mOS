b3d0beabaa84d14579762925171a9b52e428c4d3
Moldytzu <m.alex123@yahoo.com>
2023-07-16 21:57:43 +0300

    sys_close: remove unnecessary check

--- kernel/src/sys/close.c
+++ kernel/src/sys/close.c
@@ -4,8 +4,6 @@
 // (rsi = fd)
 void close(uint64_t fd, uint64_t rbx, uint64_t r8, uint64_t r9, sched_task_t *task)
 {
-    if (fd == 0)
-        return;
-
+    // todo: add safe-guards for apps to not give arbritary file descriptors (maybe give apps numerical ids?????)
     vfsClose(fd); // close the node
 }
