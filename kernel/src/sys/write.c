c9b5468364fc6056f67183c91e53e5025b1e547b
Moldytzu <m.alex123@yahoo.com>
2023-07-24 13:57:46 +0300

    sys_write: respect count argument until now we just wrote to serial console everything up to null

--- kernel/src/sys/write.c
+++ kernel/src/sys/write.c
@@ -1,6 +1,7 @@
 #include <sys/sys.h>
 #include <fs/vfs.h>
 #include <subsys/vt.h>
+#include <drv/serial.h>
 
 // write (rsi = buffer, rdx = count, r8 = fd)
 void write(uint64_t buffer, uint64_t count, uint64_t fd, uint64_t r9, sched_task_t *task)
@@ -10,8 +11,9 @@ void write(uint64_t buffer, uint64_t count, uint64_t fd, uint64_t r9, sched_task
 
     const char *charBuffer = (const char *)PHYSICAL(buffer); // get physical address of the buffer
 
-    if (task->isDriver)
-        printks("%s", charBuffer);
+    if (task->isDriver) // log driver messsages on serial
+        for (size_t i = 0; i < count; i++)
+            serialWritec(charBuffer[i]);
 
     if (fd == SYS_STDIN)
     {
