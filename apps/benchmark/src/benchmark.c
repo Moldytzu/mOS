8a5d19e8c30c6a7a671e6b68090a3535d393a57e
Moldytzu <m.alex123@yahoo.com>
2023-07-26 12:39:02 +0300

    benchmark: abstract calculation system

--- apps/benchmark/src/benchmark.c
+++ apps/benchmark/src/benchmark.c
@@ -3,10 +3,24 @@
 #include <stdio.h>
 #include <stdbool.h>
 
+// todo: move this in a dedicated library
+// todo: before doing that, create a context system
+uint64_t benchmarkStartNanos;
+
+void benchmarkStart()
+{
+    benchmarkStartNanos = sys_time_uptime_nanos();
+}
+
+uint64_t benchmarkEnd()
+{
+    uint64_t difference = sys_time_uptime_nanos() - benchmarkStartNanos;
+    uint64_t miliseconds = difference / 1000000; // difference is in nanoseconds
+    return miliseconds;
+}
+
 #define MAX_PRIME 100000
 #define PAGES 32768 // 128 megabytes
-
-volatile int count = 0;
 
 // slow prime checker
 bool isPrime(uint32_t n)
@@ -23,27 +37,37 @@ bool isPrime(uint32_t n)
     return true;
 }
 
-int main(int argc, char **argv)
+void primes()
 {
-    uint64_t a, b, c, unused;
+    volatile size_t count = 0;
 
-    count = 0;
-    a = sys_time_uptime_nanos();
-
+    benchmarkStart();
     // check for primes
     for (volatile int i = 2; i < MAX_PRIME; i++)
     {
         if (isPrime(i))
             count++;
     }
+    uint64_t miliseconds = benchmarkEnd();
 
-    b = sys_time_uptime_nanos();
+    printf("found %d primes up to %d in %llu miliseconds\n", count, MAX_PRIME, miliseconds);
+}
 
+void allocation()
+{
+    benchmarkStart();
+
+    uint64_t unused;
     for (volatile int i = 0; i < PAGES; i++)
         sys_mem(SYS_MEM_ALLOCATE, (uint64_t)&unused, 0);
 
-    c = sys_time_uptime_nanos();
+    uint64_t miliseconds = benchmarkEnd();
 
-    printf("found %d primes up to %d in %llu miliseconds\n", count, MAX_PRIME, (b - a) / 1000000);
-    printf("allocated %d pages in %llu miliseconds\n", PAGES, (c - b) / 1000000);
+    printf("allocated %d pages in %llu miliseconds\n", PAGES, miliseconds);
+}
+
+int main(int argc, char **argv)
+{
+    primes();
+    allocation();
 }
