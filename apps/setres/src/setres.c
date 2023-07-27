66fe5376f17339f1688966aa6f2bd0169a0e5a97
Moldytzu <m.alex123@yahoo.com>
2023-07-27 18:44:08 +0300

    setres: poll resolution

--- apps/setres/src/setres.c
+++ apps/setres/src/setres.c
@@ -11,12 +11,29 @@ int main(int argc, char **argv)
     }
 
     uint64_t oldX, oldY, newX, newY;
+    uint64_t targetX = atoi(argv[1]);
+    uint64_t targetY = atoi(argv[2]);
+
     sys_display(SYS_DISPLAY_GET, (uint64_t)&oldX, (uint64_t)&oldY); // get old resolution
+    sys_display(SYS_DISPLAY_SET, targetX, targetY);                 // set new resolution
 
-    sys_display(SYS_DISPLAY_SET, atoi(argv[1]), atoi(argv[2])); // set new resolution
+    int i = 0;
+    for (; i < 20; i++) // wait for the resolution to change
+    {
+        uint64_t currentX, currentY;
+        sys_display(SYS_DISPLAY_GET, (uint64_t)&currentX, (uint64_t)&currentY); // get current resolution
 
-    for (int i = 0; i < 10; i++) // wait for the resolution to change
+        if (currentX == targetX && currentY == targetY) // if resolution matches then we can break
+            break;
+
         sys_yield();
+    }
+
+    if (i == 20)
+    {
+        printf("Resolution change timed out. (no framebuffer driver running?)\n");
+        abort();
+    }
 
     sys_display(SYS_DISPLAY_GET, (uint64_t)&newX, (uint64_t)&newY); // get new resolution
     printf("Old resolution: %dx%d\nNew resolution: %dx%d\n", oldX, oldY, newX, newY);
