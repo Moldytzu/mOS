b909ab59fc193873f99fa1c0ba28283edcd74706
Moldytzu <m.alex123@yahoo.com>
2023-08-05 17:37:16 +0300

    gdt: comment load function better

--- kernel/src/cpu/gdtLoad.asm
+++ kernel/src/cpu/gdtLoad.asm
@@ -5,14 +5,17 @@ global gdtLoad
 ; load the gdt
 gdtLoad:
     lgdt [rdi] ; load gdt from the first argument
+    
     mov ax, (8*2) ; 2nd segment, kernel data
     mov ds, ax
     mov es, ax
     mov fs, ax
     mov ss, ax
     mov gs, ax
-    pop rdi
+    
+    pop rdi        ; pop return address
     mov rax, (8*1) ; 1st segment, kernel code
-    push rax
-    push rdi
-    retfq
+    
+    push rax ; push the new code segment
+    push rdi ; push the return address
+    retfq    ; do the far return
