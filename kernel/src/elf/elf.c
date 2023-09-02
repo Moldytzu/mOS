11bb8809c46b8b9d4081c31c0b8885ec743fecf9
Moldytzu <m.alex123@yahoo.com>
2023-09-02 20:37:08 +0300

    elf: determine actual in-memory size of executable saves quite a lot of memory

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -42,16 +42,11 @@ sched_task_t *elfLoad(const char *path, int argc, char **argv, bool driver)
     logDbg(LOG_SERIAL_ONLY, "elf: found %s at 0x%p with the entry offset at 0x%p", path, elf, elf->e_entry - TASK_BASE_ADDRESS);
 #endif
 
-    fdSize = align(fdSize, 4096); // align to next page
-
-    // fixme: the fdSize might not represent the actual size in memory of the executable
-    // we should calculate the full size of all program headers' memsize
-    void *buffer = pmmPages(fdSize / VMM_PAGE); // allocate the buffer for the sections
-
+    // determine in-memory size of executable
     Elf64_Phdr *phdr = blkBlock(elf->e_ehsize);     // buffer to store information about the current program header
     vfsRead(fd, phdr, elf->e_ehsize, elf->e_phoff); // read first header
 
-    // todo: if we find the .bss section zero it to gurantee the app that it's clear
+    size_t memsz = 0;
     for (int i = 0; i < elf->e_phnum; i++) // iterate over every program header
     {
         if (phdr->p_type == PT_LOAD) // section to be loaded
@@ -59,6 +54,20 @@ sched_task_t *elfLoad(const char *path, int argc, char **argv, bool driver)
 #ifdef K_ELF_DEBUG
             logDbg(LOG_SERIAL_ONLY, "elf: phdr at virtual 0x%p (physical 0x%p) with size %d bytes", phdr->p_vaddr, phdr->p_paddr, phdr->p_memsz);
 #endif
+            memsz += phdr->p_memsz;
+        }
+
+        vfsRead(fd, phdr, elf->e_ehsize, elf->e_phoff + i); // read next program header
+    }
+    memsz = align(memsz, PMM_PAGE); // align to page size
+
+    // load program headers
+    void *buffer = pmmPages(memsz / PMM_PAGE); // allocate the buffer for the sections
+
+    for (int i = 0; i < elf->e_phnum; i++) // iterate over every program header
+    {
+        if (phdr->p_type == PT_LOAD) // program header to be loaded
+        {
             vfsRead(fd, (void *)((uint64_t)buffer + phdr->p_vaddr - TASK_BASE_ADDRESS), phdr->p_filesz, phdr->p_offset); // copy the program header to the buffer using the vfs
         }
 
@@ -67,12 +76,13 @@ sched_task_t *elfLoad(const char *path, int argc, char **argv, bool driver)
 
     blkDeallocate(phdr); // clean up
 
+    // copy cwd
     char *cwd = pmmPage();
     memcpy(cwd, path, strlen(path)); // copy the path
     for (int i = strlen(cwd) - 1; cwd[i] != '/'; cwd[i--] = '\0')
         ; // step back to last delimiter (removes file name)
 
-    sched_task_t *task = schedAdd(path, (void *)elf->e_entry - TASK_BASE_ADDRESS, K_STACK_SIZE, buffer, fdSize, 0, cwd, argc, argv, true, driver); // add the task                                                                                                                         // free
+    sched_task_t *task = schedAdd(path, (void *)elf->e_entry - TASK_BASE_ADDRESS, K_STACK_SIZE, buffer, memsz, 0, cwd, argc, argv, true, driver); // add the task                                                                                                                         // free
     pmmDeallocate(cwd);
     blkDeallocate(elf);
     return task; // return the task
