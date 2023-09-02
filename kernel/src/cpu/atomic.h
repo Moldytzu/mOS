1fe38e4f933b1b7f93195f1c98364edade143761
Moldytzu <m.alex123@yahoo.com>
2023-09-02 21:51:41 +0300

    atomic: typedef locker as uint64_t

--- kernel/src/cpu/atomic.h
+++ kernel/src/cpu/atomic.h
@@ -1,10 +1,7 @@
 #pragma once
 #include <stdbool.h>
 
-typedef struct __attribute__((__packed__))
-{
-    uint8_t mutex;
-} locker_t;
+typedef uint64_t locker_t;
 
 void atomicWrite(void *address, uint64_t val);
 bool atomicRelease(locker_t *locker);
@@ -13,5 +10,5 @@ void atomicAquire(locker_t *locker);
 // hint spinlock
 static inline __attribute__((always_inline)) void pause()
 {
-    asm volatile ("pause");
+    asm volatile("pause");
 }
