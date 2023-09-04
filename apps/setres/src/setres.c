db106be329f0ef31f5d5b3095d11d0511d7f6a60
Moldytzu <m.alex123@yahoo.com>
2023-09-04 22:01:39 +0300

    setres: clean up algorithm

--- apps/setres/src/setres.c
+++ apps/setres/src/setres.c
@@ -14,27 +14,19 @@ int main(int argc, char **argv)
     uint64_t targetX = atoi(argv[1]);
     uint64_t targetY = atoi(argv[2]);
 
-    sys_display(SYS_DISPLAY_GET, (uint64_t)&oldX, (uint64_t)&oldY); // get old resolution
-    sys_display(SYS_DISPLAY_SET, targetX, targetY);                 // set new resolution
+    sys_display(SYS_DISPLAY_GET, (uint64_t)&oldX, (uint64_t)&oldY);   // get old resolution
+    uint64_t status = sys_display(SYS_DISPLAY_SET, targetX, targetY); // set new resolution
 
-    int i = 0;
-    for (; i < 50; i++) // wait for the resolution to change
+    if (status) // failure
     {
-        uint64_t currentX, currentY;
-        sys_display(SYS_DISPLAY_GET, (uint64_t)&currentX, (uint64_t)&currentY); // get current resolution
-
-        if (currentX == targetX && currentY == targetY) // if resolution matches then we can break
-            break;
-
-        sys_yield();
-    }
-
-    if (i == 20)
-    {
-        printf("Resolution change timed out. (no framebuffer driver running?)\n");
+        printf("Failed to set resolution. No framebuffer driver found.\n");
         abort();
     }
+    else // success
+    {
+        sys_display(SYS_DISPLAY_GET, (uint64_t)&newX, (uint64_t)&newY); // get new resolution
+        printf("Old resolution: %dx%d\nNew resolution: %dx%d\n", oldX, oldY, newX, newY);
+    }
 
-    sys_display(SYS_DISPLAY_GET, (uint64_t)&newX, (uint64_t)&newY); // get new resolution
-    printf("Old resolution: %dx%d\nNew resolution: %dx%d\n", oldX, oldY, newX, newY);
+    return 0;
 }
