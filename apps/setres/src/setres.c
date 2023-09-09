1bdb1d20864cc0b1b2352027ffeb4a6a434ee92f
Moldytzu <m.alex123@yahoo.com>
2023-09-09 20:21:57 +0300

    setres: timeout for resolution change

--- apps/setres/src/setres.c
+++ apps/setres/src/setres.c
@@ -17,6 +17,12 @@ int main(int argc, char **argv)
     sys_display(SYS_DISPLAY_GET, (uint64_t)&oldX, (uint64_t)&oldY);   // get old resolution
     uint64_t status = sys_display(SYS_DISPLAY_SET, targetX, targetY); // set new resolution
 
+    if (oldX == targetX && oldY == targetY)
+    {
+        printf("No resolution to change.\n");
+        return 0;
+    }
+
     if (status) // failure
     {
         printf("Failed to set resolution. No framebuffer driver found.\n");
@@ -24,8 +30,22 @@ int main(int argc, char **argv)
     }
     else // success
     {
-        sys_display(SYS_DISPLAY_GET, (uint64_t)&newX, (uint64_t)&newY); // get new resolution
-        printf("Old resolution: %dx%d\nNew resolution: %dx%d\n", oldX, oldY, newX, newY);
+        // wait for resolution change
+        for (int i = 0; i < 50; i++)
+        {
+            sys_yield();
+            sys_display(SYS_DISPLAY_GET, (uint64_t)&newX, (uint64_t)&newY); // get new resolution
+
+            if (newX != oldX || newY != oldY) // resolution has changed
+            {
+                printf("Old resolution: %dx%d\nNew resolution: %dx%d\n", oldX, oldY, newX, newY);
+                return 0;
+            }
+        }
+
+        // it didn't change
+        printf("Resolution change timed out.\n");
+        abort();
     }
 
     return 0;
