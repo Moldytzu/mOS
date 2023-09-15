f3f093eefe37c39fbb06106f15366c0765370054
Moldytzu <m.alex123@yahoo.com>
2023-09-15 17:44:26 +0300

    crash: user interface :^) can now choose betwen page fault and divide by zero

--- apps/crash/src/crash.c
+++ apps/crash/src/crash.c
@@ -4,8 +4,28 @@
 
 int main(int argc, char **argv)
 {
-    puts("Writing garbage to NULL.\n");
+    puts("Crash\nPress 1 - Page Fault\nPress 2 - Divide By Zero\n");
 
-    uint64_t *null = NULL;
-    *null = 0xdeadbeef;
+    char c;
+    do
+    {
+        c = sys_input_keyboard();
+    } while (!c || c <= '0' || c >= '3');
+
+    switch (c)
+    {
+    case '1':
+        puts("Writing garbage to NULL.\n");
+
+        uint64_t *null = NULL;
+        *null = 0xdeadbeef;
+        break;
+    case '2':
+        puts("Dividing 5 by zero\n");
+
+        int a = 0;
+        int b = 5;
+        volatile int c = b / a;
+        break;
+    }
 }
