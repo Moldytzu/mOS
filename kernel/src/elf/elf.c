db7dafc91a71f85d657323a93424e8d62e565132
Moldytzu <m.alex123@yahoo.com>
2023-09-17 20:24:12 +0300

    elf: determine virtual base address

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -20,7 +20,7 @@ bool elfIsCompatible(Elf64_Ehdr *elf)
            elf->e_version == EV_CURRENT;           // check version
 }
 
-// load an elf
+// load a static elf binary as a userspace task
 sched_task_t *elfLoad(const char *path, int argc, char **argv, bool driver)
 {
     uint64_t fd = vfsOpen(path);                    // open the file
@@ -42,7 +42,9 @@ sched_task_t *elfLoad(const char *path, int argc, char **argv, bool driver)
     logDbg(LOG_SERIAL_ONLY, "elf: found %s at 0x%p with the entry offset at 0x%p", path, elf, elf->e_entry - TASK_BASE_ADDRESS);
 #endif
 
-    // determine in-memory size of executable (pass 1)
+    uint64_t virtualBase = UINT64_MAX; // base virtual address of the executable in memory
+
+    // determine in-memory size of executable and virtual base (pass 1)
     Elf64_Phdr *phdr = blkBlock(elf->e_ehsize);     // buffer to store information about the current program header
     vfsRead(fd, phdr, elf->e_ehsize, elf->e_phoff); // read first header
 
@@ -55,6 +57,9 @@ sched_task_t *elfLoad(const char *path, int argc, char **argv, bool driver)
             logDbg(LOG_SERIAL_ONLY, "elf: phdr at virtual 0x%p (physical 0x%p) with size %d bytes", phdr->p_vaddr, phdr->p_paddr, phdr->p_memsz);
 #endif
             memsz += phdr->p_memsz;
+
+            if (phdr->p_vaddr < virtualBase) // find lowest address possible
+                virtualBase = phdr->p_vaddr;
         }
 
         vfsRead(fd, phdr, elf->e_ehsize, elf->e_phoff + i); // read next program header
@@ -69,7 +74,7 @@ sched_task_t *elfLoad(const char *path, int argc, char **argv, bool driver)
     {
         if (phdr->p_type == PT_LOAD) // program header to be loaded
         {
-            vfsRead(fd, (void *)((uint64_t)buffer + phdr->p_vaddr - TASK_BASE_ADDRESS), phdr->p_filesz, phdr->p_offset); // copy the program header to the buffer using the vfs
+            vfsRead(fd, (void *)((uint64_t)buffer + phdr->p_vaddr - virtualBase), phdr->p_filesz, phdr->p_offset); // copy the program header to the buffer using the vfs
         }
 
         vfsRead(fd, phdr, elf->e_ehsize, elf->e_phoff + i); // read next program header
@@ -83,7 +88,10 @@ sched_task_t *elfLoad(const char *path, int argc, char **argv, bool driver)
     for (int i = strlen(cwd) - 1; cwd[i] != '/'; cwd[i--] = '\0')
         ; // step back to last delimiter (removes file name)
 
-    sched_task_t *task = schedAdd(path, (void *)elf->e_entry - TASK_BASE_ADDRESS, K_STACK_SIZE, buffer, memsz, 0, cwd, argc, argv, true, driver); // add the task                                                                                                                         // free
+    // add the task (fixme: scheduler expects the virtual base address to be at TASK_BASE_ADDRESS, make it take it as an argument)
+    sched_task_t *task = schedAdd(path, (void *)elf->e_entry - virtualBase, K_STACK_SIZE, buffer, memsz, 0, cwd, argc, argv, true, driver);
+
+    // clean up
     pmmDeallocate(cwd);
     blkDeallocate(elf);
     return task; // return the task
