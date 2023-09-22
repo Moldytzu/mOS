5f7db2dc4013826c180206fc6c3ae623aacb37a9
Moldytzu <m.alex123@yahoo.com>
2023-09-22 15:25:27 +0300

    isr: clean up error code from the stack fixes some stabliity issues and crashes

--- kernel/src/cpu/isr.asm
+++ kernel/src/cpu/isr.asm
@@ -12,16 +12,14 @@ global __baseHandler%1
 
 __baseHandler%1:
 %ifn %1 == 0x8 || %1 == 0xA || %1 == 0xB || %1 == 0xC || %1 == 0xD || %1 == 0xE || %1 == 0x11 || %1 == 0x15 || %1 == 0x1D || %1 == 0x1E
-    push rax             ; push an arbitrary error code
+    sub rsp, 8 ; allocate an error code on the stack if the cpu doesn't do it for us 
 %endif
     PUSH_REG              ; save old registers
     mov rdi, rsp          ; give the handler the stack frame
     mov rsi, %1           ; give the intrerrupt number
     call exceptionHandler ; call the handler
     POP_REG               ; restore registers
-%ifn %1 == 0x8 || %1 == 0xA || %1 == 0xB || %1 == 0xC || %1 == 0xD || %1 == 0xE || %1 == 0x11 || %1 == 0x15 || %1 == 0x1D || %1 == 0x1E
-    pop rax               ; push an arbitrary error code
-%endif
+    add rsp, 8            ; clean up error code on the stack
     iretq                 ; return to previous context
 %endmacro
 
