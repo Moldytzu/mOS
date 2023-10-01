17dcf6c1d2889a88b354ed66887a372790554be6
Moldytzu <m.alex123@yahoo.com>
2023-10-01 19:38:41 +0300

    sys_mailbox: syscall to compose

--- kernel/src/sys/mailbox.c
+++ kernel/src/sys/mailbox.c
@@ -1,11 +1,10 @@
 #include <sys/sys.h>
 
-uint64_t mailbox(uint64_t call, uint64_t rdx, uint64_t r8, uint64_t r9, sched_task_t *task)
+uint64_t mailbox(uint64_t call, uint64_t arg1, uint64_t arg2, uint64_t arg3, sched_task_t *task)
 {
     switch (call)
     {
     case 0: // read last
-
         // read next mail
         mailbox_t *mail = mailReadNext(&task->mailbox);
 
@@ -19,10 +18,21 @@ uint64_t mailbox(uint64_t call, uint64_t rdx, uint64_t r8, uint64_t r9, sched_ta
         vmmMap(task->pageTable, virtualStart, mail->contents, VMM_ENTRY_RO | VMM_ENTRY_USER);
 
         // fixme: add returned pages to allocated buffer to prevent potential memory leaks in case the app doesn't free the mail
-        // a new helper function would help
 
         return (uint64_t)virtualStart;
+
+    case 1: // compose
+        sched_task_t *destination = schedGet(arg1);
+        if (!destination)
+            return SYSCALL_STATUS_ACCESS_DENIED;
+
+        if (!IS_MAPPED(arg2))
+            return SYSCALL_STATUS_ACCESS_DENIED;
+
+        mailCompose(&destination->mailbox, task->id, 1, PHYSICAL(arg2), arg3); // fixme: we shouldn't trust arg3 here, also we should take a subject too
+
+        return SYSCALL_STATUS_OK;
     default:
-        return 0;
+        return SYSCALL_STATUS_UNKNOWN_OPERATION;
     }
 }
